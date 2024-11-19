8930b7908a5bea74bcaf134d3beda5b8719f149a
dhruv kaushik <dhruvkaushik305@gmail.com>
2024-11-19 16:43:32 +0530

    created an elb

--- modules/terraform-aws-network/variables.tf
+++ modules/terraform-aws-network/variables.tf
@@ -65,3 +65,20 @@ variable "private_app_sg_rule_egress" {
 variable "public_instance_id" {
   type = string
 }
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "elb_listeners" {
+  type = list(object({
+    instance_port     = number
+    instance_protocol = string
+    lb_port           = number
+    lb_protocol       = string
+  }))
+}
